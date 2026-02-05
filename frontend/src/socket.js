ba82f23173e2413aa1ac4c1dd569f7b44b253463
JonFRutan <rutanj@vcu.edu>
2026-02-04 21:45:30 -0500

    Moved useLocalHost to rely on an environment variable

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,7 +3,7 @@
 import { io } from 'socket.io-client';
 
 // EDIT THIS TO CHANGE BETWEEN SERVER DEPLOYMENT AND LOCAL DEV ENVIRONMENT
-const useLocalhost = true;
+const useLocalhost = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 // --------------------------------------------------------
 
 const socket = io(
